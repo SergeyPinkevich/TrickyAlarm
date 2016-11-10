f8d2adec44e6a7cebd041605263ce509f082e2a1
Nikolay <bul-nick-al@ya.ru>
2016-11-10 16:55:28 +0300

    Nick

---
+++ app/src/main/java/com/example/trickyalarm/CardAlarmAdapter.java
@@ -0,0 +1,85 @@
+package com.example.trickyalarm;
+
+/**
+ * Created by dns on 10.11.2016.
+ */
+
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+
+class CardAlarmAdapter extends RecyclerView.Adapter<CardAlarmAdapter.ViewHolder>{
+    //Предоставляет ссылку на представления, используемые в RecyclerView
+
+    private String[] dates;
+    private boolean[] checkers;
+    private String[] days;
+    private String[] timeLeft;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private CardView cardView;
+
+        public ViewHolder (CardView v){
+            super(v);
+            cardView = v;
+        }
+    }
+
+    public CardAlarmAdapter (String[] dates, boolean[] checkers, String[] days, String[] timeLeft){
+        this.dates = dates;
+        this.checkers = checkers;
+        this.days = days;
+        this.timeLeft = timeLeft;
+    }
+
+    @Override
+    public CardAlarmAdapter.ViewHolder onCreateViewHolder(
+            ViewGroup parent, int viewType){
+        CardView cv = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.alarm_card, parent, false);
+        return new ViewHolder(cv);
+    }
+
+    @Override public void onBindViewHolder(ViewHolder holder, int position){
+
+
+        CardView cardView = holder.cardView;
+
+        cardView.setCardElevation(0);
+        Switch switch1 = (Switch) cardView.findViewById(R.id.switch1);
+        switch1.setChecked(checkers[position]);
+
+        TextView time = (TextView) cardView.findViewById(R.id.time_main);
+        time.setText(dates[position]);
+
+        TextView daysView = (TextView) cardView.findViewById(R.id.days);
+        daysView.setText(days[position]);
+
+        TextView timeLeftView = (TextView) cardView.findViewById(R.id.timeLeft);
+        timeLeftView.setText(timeLeft[position]);
+
+        RelativeLayout relativeLayout = (RelativeLayout) cardView.findViewById(R.id.layout);
+
+
+        if (dates[position] == "01:20"){
+            relativeLayout.setBackgroundResource(R.drawable.p1);
+        }
+        if (dates[position] == "9:00"){
+            relativeLayout.setBackgroundResource(R.drawable.p2);
+        }
+        if (dates[position] == "13:20"){
+            relativeLayout.setBackgroundResource(R.drawable.p3);
+        }
+
+    }
+
+    @Override public int getItemCount()
+    {
+        return dates.length;
+
+    }
+}
--- app/src/main/java/com/example/trickyalarm/MainActivity.java
+++ app/src/main/java/com/example/trickyalarm/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.trickyalarm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.alarm_list);
+        String[] alarms = {"9:00","9:00","9:00","01:20","13:20","9:00","01:20","13:20","9:00","13:20"};
+        boolean[] checkers = {true, false,true, false,true, false,true, false,true, false};
+        String[] days = {"Mon","Weekday","Tue, Sat","Weekend","Wed, Fri","Mon","Weekday","Tue, Sat","Weekend","Wed, Fri"};
+        String[] timeLeft = {"(20 Min)","(14 Min)","(39 Min)","(5 Min)","(20 min)","(20 Min)","(14 Min)","(39 Min)","(5 Min)","(20 min)"};
+
+        CardAlarmAdapter adapter = new CardAlarmAdapter(alarms, checkers, days, timeLeft);
+
+        recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
