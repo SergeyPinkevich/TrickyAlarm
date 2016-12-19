18a57daf711ca9daf3759ebdf330c1157c713951
“SergeyPinkevich” <sergeipinkevich163@gmail.com>
2016-12-19 17:23:09 +0300

    customize toolbar

--- app/src/main/java/com/example/trickyalarm/MainActivity.java
+++ app/src/main/java/com/example/trickyalarm/MainActivity.java
@@ -22,26 +22,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mCustomFont = Typeface.createFromAsset(getAssets(), "fonts/Exo2-Thin.ttf");
+
+        customizeToolbar();
+
+//        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.alarm_list);
+//        String[] alarms = {"9:00","9:00","9:00","01:20","13:20","9:00","01:20","13:20","9:00","13:20"};
+//        boolean[] checkers = {true, false,true, false,true, false,true, false,true, false};
+//        String[] days = {"Mon","Weekday","Tue, Sat","Weekend","Wed, Fri","Mon","Weekday","Tue, Sat","Weekend","Wed, Fri"};
+//        String[] timeLeft = {"(20 Min)","(14 Min)","(39 Min)","(5 Min)","(20 min)","(20 Min)","(14 Min)","(39 Min)","(5 Min)","(20 min)"};
+//
+//        CardAlarmAdapter adapter = new CardAlarmAdapter(alarms, checkers, days, timeLeft);
+//
+//        recyclerView.setAdapter(adapter);
+//
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//        recyclerView.setLayoutManager(layoutManager);
+    }
+
+    public void customizeToolbar() {
         mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar);
         mToolbarTitle = (TextView) mActionBarToolbar.findViewById(R.id.toolbar_title);
-        mToolbarTitle.setText("Tricky Alarm");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
-        mCustomFont = Typeface.createFromAsset(getAssets(), "fonts/Exo2-Thin.ttf");
+        setSupportActionBar(mActionBarToolbar);
+        mToolbarTitle.setText(mActionBarToolbar.getTitle());
+
         mToolbarTitle.setTypeface(mCustomFont);
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.alarm_list);
-        String[] alarms = {"9:00","9:00","9:00","01:20","13:20","9:00","01:20","13:20","9:00","13:20"};
-        boolean[] checkers = {true, false,true, false,true, false,true, false,true, false};
-        String[] days = {"Mon","Weekday","Tue, Sat","Weekend","Wed, Fri","Mon","Weekday","Tue, Sat","Weekend","Wed, Fri"};
-        String[] timeLeft = {"(20 Min)","(14 Min)","(39 Min)","(5 Min)","(20 min)","(20 Min)","(14 Min)","(39 Min)","(5 Min)","(20 min)"};
-
-        CardAlarmAdapter adapter = new CardAlarmAdapter(alarms, checkers, days, timeLeft);
-
-        recyclerView.setAdapter(adapter);
-
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
     @Override
