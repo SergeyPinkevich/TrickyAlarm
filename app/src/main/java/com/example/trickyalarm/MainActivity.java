12b115a96c4970aea59149cf4c41187c2fb7a292
“SergeyPinkevich” <sergeipinkevich163@gmail.com>
2016-12-22 17:01:29 +0300

    merge dev and Serg

--- app/src/main/java/com/example/trickyalarm/MainActivity.java
+++ app/src/main/java/com/example/trickyalarm/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.trickyalarm;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Typeface;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,11 +34,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AlarmReceiver alarmReceiver = new AlarmReceiver();
-        alarmReceiver.setAlarm(this.getApplicationContext(), (int) (System.currentTimeMillis() + 5 * 1000));
-
-        mCustomFont = Typeface.createFromAsset(getAssets(), "fonts/Exo2-Thin.ttf");
-        Typeface fontForCard = Typeface.createFromAsset(getAssets(), "fonts/Exo2-Light.ttf");
+        mCustomFont = Typeface.createFromAsset(getAssets(), "fonts/Exo2-Light.ttf");
 
         customizeToolbar();
 
@@ -37,14 +42,13 @@ public class MainActivity extends AppCompatActivity {
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.alarm_list);
 
-        CardAlarmAdapter adapter = new CardAlarmAdapter(alarms, fontForCard);
+        CardAlarmAdapter adapter = new CardAlarmAdapter(alarms, mCustomFont, this);
 
         recyclerView.setAdapter(adapter);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
     }
-
 
     public void readFromDatabase() {
         alarms = new ArrayList<>();
@@ -55,10 +59,13 @@ public class MainActivity extends AppCompatActivity {
             mCursor = mDatabase.query(SimpleDatabaseHelper.TABLE_NAME, null, null, null, null, null, null);
             if (mCursor.moveToFirst()) {
                 while (mCursor.isAfterLast() == false) {
-                    Alarm temp = new Alarm(mCursor.getInt(0), mCursor.getLong(1), mCursor.getInt(2),
-                            mCursor.getInt(3), mCursor.getInt(4), mCursor.getInt(5), mCursor.getInt(6),
-                            mCursor.getInt(7), mCursor.getInt(8), mCursor.getInt(9), mCursor.getInt(10),
-                            mCursor.getInt(11));
+                    long milliseconds = mCursor.getLong(2);
+                    Alarm temp = new Alarm(intToBoolean(mCursor.getInt(1)), getCalendarFromMilliseconds(milliseconds),
+                            mCursor.getInt(3), intToBoolean(mCursor.getInt(3)),
+                            intToBoolean(mCursor.getInt(4)), intToBoolean(mCursor.getInt(5)),
+                            intToBoolean(mCursor.getInt(6)), intToBoolean(mCursor.getInt(7)),
+                            intToBoolean(mCursor.getInt(8)), intToBoolean(mCursor.getInt(9)),
+                            intToBoolean(mCursor.getInt(10)), mCursor.getInt(11));
                     alarms.add(temp);
                     mCursor.moveToNext();
                 }
@@ -67,6 +74,16 @@ public class MainActivity extends AppCompatActivity {
         } catch (SQLiteException e) {
             Toast.makeText(this, "Database is unavailable", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public boolean intToBoolean(int number) {
+        return number > 0 ? true : false;
+    }
+
+    public Calendar getCalendarFromMilliseconds(long milliseconds) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(milliseconds);
+        return calendar;
     }
 
     public void close() {
@@ -101,5 +118,4 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
-    
 }
