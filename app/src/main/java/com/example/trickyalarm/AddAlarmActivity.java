004b621c16402e0142da267d27351cc73357c05f
“SergeyPinkevich” <sergeipinkevich163@gmail.com>
2016-12-23 18:41:59 +0300

    remove test records

--- app/src/main/java/com/example/trickyalarm/AddAlarmActivity.java
+++ app/src/main/java/com/example/trickyalarm/AddAlarmActivity.java
@@ -1,27 +1,53 @@
 package com.example.trickyalarm;
 
 import android.graphics.Typeface;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 
 import com.android.datetimepicker.time.RadialPickerLayout;
 import com.android.datetimepicker.time.TimePickerDialog;
 
+import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class AddAlarmActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+public class AddAlarmActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener, View.OnClickListener {
 
     private static final String TIME_PATTERN = "HH:mm";
 
     private Toolbar mActionBarToolbar;
     private TextView mToolbarTitle;
     private Typeface mCustomFont;
+    private TextView lblTextTime;
+    private TextView lblBias;
+    private TextView lblRepeat;
+    private TextView lblWeekly;
+
+    private boolean[] daysConditions = new boolean[7];
+
+    private Button onMonday;
+    private Button onTuesday;
+    private Button onWednesday;
+    private Button onThursday;
+    private Button onFriday;
+    private Button onSaturday;
+    private Button onSunday;
+    private Button confirm;
+
+    private ToggleButton repeat;
+    private DiscreteSeekBar bias;
+    private DiscreteSeekBar interval;
+
+    private View.OnClickListener weekButtons;
 
     private TextView lblTime;
     private Calendar calendar;
@@ -37,7 +63,47 @@ public class AddAlarmActivity extends AppCompatActivity implements TimePickerDia
 
         lblTime = (TextView) findViewById(R.id.lblTime);
 
-        mCustomFont = Typeface.createFromAsset(getAssets(), "fonts/Exo2-Thin.ttf");
+        mCustomFont = Typeface.createFromAsset(getAssets(), "fonts/Exo2-Light.ttf");
+
+        lblTextTime = (TextView) findViewById(R.id.lblTextTime);
+        lblTextTime.setTypeface(mCustomFont);
+
+        lblTime.setTypeface(mCustomFont);
+
+        lblTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openTimePicker();
+            }
+        });
+
+        lblBias = (TextView) findViewById(R.id.lblBias);
+        lblBias.setTypeface(mCustomFont);
+
+        lblRepeat = (TextView) findViewById(R.id.lblRepeat);
+        lblRepeat.setTypeface(mCustomFont);
+
+        lblWeekly = (TextView) findViewById(R.id.lblWeekly);
+        lblWeekly.setTypeface(mCustomFont);
+
+        onMonday = (Button) findViewById(R.id.monday_letter);
+        onTuesday = (Button) findViewById(R.id.tuesday_letter);
+        onWednesday = (Button) findViewById(R.id.wednesday_letter);
+        onThursday = (Button) findViewById(R.id.thursday_letter);
+        onFriday = (Button) findViewById(R.id.friday_letter);
+        onSaturday = (Button) findViewById(R.id.saturday_letter);
+        onSunday = (Button) findViewById(R.id.sunday_letter);
+
+        repeat = (ToggleButton) findViewById(R.id.toggle_button);
+
+        bias = (DiscreteSeekBar) findViewById(R.id.discreteSeekBarBias);
+
+        interval = (DiscreteSeekBar) findViewById(R.id.discreteSeekBarInterval);
+
+        confirm = (Button) findViewById(R.id.add_alarm_confirm);
+
+        onMonday.setOnClickListener(this);
+        confirm.setOnClickListener(this);
 
         customizeToolbar();
 
@@ -48,11 +114,10 @@ public class AddAlarmActivity extends AppCompatActivity implements TimePickerDia
         lblTime.setText(timeFormat.format(calendar.getTime()));
     }
 
-//    public void onClick(View view) {
-//        if (view.getId() == R.id.btnTimePicker) {
-//            TimePickerDialog.newInstance(this, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show(getFragmentManager(), "timePicker");
-//        }
-//    }
+
+    public void openTimePicker() {
+        TimePickerDialog.newInstance(this, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show(getFragmentManager(), "timePicker");
+    }
 
     @Override
     public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
@@ -61,15 +126,57 @@ public class AddAlarmActivity extends AppCompatActivity implements TimePickerDia
         update();
     }
 
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
+    }
+
     public void customizeToolbar() {
         mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar);
         mToolbarTitle = (TextView) mActionBarToolbar.findViewById(R.id.toolbar_title);
 
         setSupportActionBar(mActionBarToolbar);
-        mToolbarTitle.setText(mActionBarToolbar.getTitle());
+        mToolbarTitle.setText(R.string.new_alarm);
 
         mToolbarTitle.setTypeface(mCustomFont);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.monday_letter:
+                setTextColor(onMonday, 0);
+                break;
+            case R.id.add_alarm_confirm:
+                addAlarm();
+                onSupportNavigateUp();
+                break;
+        }
+    }
+
+    public void setTextColor(Button button, int number) {
+        daysConditions[number] = !daysConditions[number];
+        if (daysConditions[number])
+            button.setTextColor(ContextCompat.getColor(this, R.color.white_color));
+
+        else if (!button.isEnabled())
+            button.setTextColor(ContextCompat.getColor(this, R.color.semi_transparent));
+    }
+
+    public void addAlarm() {
+        Alarm alarm;
+
+        if (repeat.isChecked())
+            alarm = new Alarm(true, calendar, bias.getProgress(), daysConditions[0], daysConditions[1], daysConditions[2],
+                    daysConditions[3], daysConditions[4], daysConditions[5], daysConditions[6], true, interval.getProgress());
+        else
+            alarm = new Alarm(true, calendar, bias.getProgress(), false, interval.getProgress());
+        SimpleDatabaseHelper mHelper = new SimpleDatabaseHelper(this);
+        mHelper.addAlarm(mHelper.getReadableDatabase(), alarm);
     }
 }
