dca38f22ecfae89d160f0012486f6f6dcd77ff73
AlmazSamatov <samatov.almaz@yandex.ru>
2016-12-29 16:03:47 +0300

    Merge branch 'development' of https://github.com/SergeyPinkevich/TrickyAlarm into Almaz
    Conflicts:
    	.gradle/2.14.1/taskArtifacts/cache.properties.lock
    	.gradle/2.14.1/taskArtifacts/fileHashes.bin
    	.gradle/2.14.1/taskArtifacts/fileSnapshots.bin
    	.gradle/2.14.1/taskArtifacts/fileSnapshotsToTreeSnapshotsIndex.bin
    	.gradle/2.14.1/taskArtifacts/taskArtifacts.bin
    	.idea/workspace.xml
    	local.properties
    
    Added fullscreen to signal activity.

--- app/src/main/java/com/example/trickyalarm/SignalActivity.java
+++ app/src/main/java/com/example/trickyalarm/SignalActivity.java
@@ -11,6 +11,8 @@ import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.example.trickyalarm.database.AlarmRepo;
@@ -30,6 +32,10 @@ public class SignalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        // remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_signal);
 
         AlarmRepo alarmRepo = new AlarmRepo(this.getApplicationContext());
